32e8be23f5562d7cd7e09cc07edbba85d106809a
Matt McCabe <matt.mccabe@attainia.com>
2014-12-13 16:39:12 -0700

    tying into firebase

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,25 +8,10 @@
  * Controller of the holidayjs2014App
  */
 angular.module('badsantaApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $firebase, FBURL) {
 
-  	$scope.santa1 = {name:'1st',imageUrl:'http://placekitten.com/g/200/300',active:false};
-  	$scope.santa2 = {name:'2nd',imageUrl:'http://placekitten.com/g/200/300',active:false};
-  	$scope.santa3 = {name:'3rd',imageUrl:'http://placekitten.com/g/200/300',active:false};
-  	$scope.santa4 = {name:'4th',imageUrl:'http://placekitten.com/g/200/300',active:false};
-  	$scope.santa5 = {name:'5th',imageUrl:'http://placekitten.com/g/200/300',active:false};
+  	var fireRef = new Firebase(FBURL);
 
-    $scope.onClickSanta = function(santaPic, bg)
-    {
-    	santaPic.active = !santaPic.active;
+  	$scope.santaPics = $firebase(fireRef).$asArray();
 
-    	if(santaPic.active)
-    	{
-    		bg = {background:'url(' + santaPic.imageUrl + ')'};
-    	}
-    	else 
-    	{
-    		bg = {};
-    	}
-    }
   });
