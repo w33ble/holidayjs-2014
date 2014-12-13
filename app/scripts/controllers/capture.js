631d86278535e7bc31bdfddf80facf06e539cf36
vageeshb <vageesh_bhasin@outlook.com>
2014-12-13 14:48:18 -0700

    Added capture

---
+++ app/scripts/controllers/capture.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name holidayjs2014App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the holidayjs2014App
+ */
+angular.module('badsantaApp')
+  .controller('CaptureCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
